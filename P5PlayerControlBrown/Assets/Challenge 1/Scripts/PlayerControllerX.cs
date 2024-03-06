98325fc51e4e5b141be803b25480ae6ccae3977c
Kayden Brown <145067189+Buried7564@users.noreply.github.com>
2024-03-06 13:43:06 -0800

    rec ready

---
+++ P5PlayerControlBrown/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float turnSpeed = 150.0f;
+    public float verticalInput;
+    public float horizontalInput;
+    public float forwardInput;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's Horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
+        transform.Rotate(Vector3.left, turnSpeed * verticalInput * Time.deltaTime);
+    }
+}
--- P5PlayerControlBrown/Assets/Scripts/PlayerController.cs
+++ P5PlayerControlBrown/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    private float speed = 10.0f;
+    private float turnSpeed = 20.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +21,16 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //Move the vehcle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * turnSpeed * horizontalInput);
+        if (Input.GetKey(KeyCode.C)) 
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+       
     }
 }
